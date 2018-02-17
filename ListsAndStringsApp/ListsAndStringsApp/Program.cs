95e0fc4cae27132973afa13860538a06b92cdf26
TatyCat <williams.tatyanna@gmail.com>
2018-02-17 12:37:23 -0500

    Lists and Strings app. Prints words that are the length of five.

---
+++ ListsAndStringsApp/ListsAndStringsApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsAndStringsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize list
+            List<string> wordList = new List<string>(new string[] {"Let", "Word", "Chunk", "Tree", "AC", "Laptop"});
+            
+            foreach (string word in wordList)
+            {
+                if(word.Length == 5)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*Write a static method to print out each word in a list that has exactly 5 letters. */
+
+    //create a list of words, at least one with exactly five letters.
+    //write a method that loops through the list to print it out if length == 5
+    //write out to console
+    //readline to keep window open.
---
+++ ListsApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a list with int 1-10
+            List<int> numbersList = new List<int>(new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
+
+            StringBuilder evenNumbers = new StringBuilder();
+
+            //loop through the list of numbers to get even ones. 
+            //if divisible by 2, return.
+            foreach (int number in numbersList)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Append(number + ", ");
+                }
+            }
+
+            evenNumbers.Remove(evenNumbers.Length - 2, 2);
+            
+            Console.Write($"I found: {evenNumbers} in the list!");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+/*Find the Sum of ALL even numbers in a list*/
+
+    //Create a list with int 1-10
+    //call the method on the list 
+
+    //loop through the list of numbers
+    //if statement, if divisible by 2, and return 
