1ea6a6e3f3a59e278e56d8cc489c549b9d2c9776
TatyCat <williams.tatyanna@gmail.com>
2018-02-17 13:03:46 -0500

    Arrays App: Creates an array, loops through and prints values.

---
+++ ArraysApp/ArraysApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArraysApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = new int[] { 1, 1, 2, 3, 5, 8 }; 
+            foreach(int number in numbersArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/* Create and initialize in a single line an array with the following values: 
+ 1, 1, 2, 3, 5, 8. Then loop through the array and print out each value. */
+
+//Initialize array with 1, 1, 2, 3, 5, 8 int values.
+//loop through array and print.
